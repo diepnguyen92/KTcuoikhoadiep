246da3ab08d3690c862318976aa69b104d6f4fea
diepnguyen92 <148781455+diepnguyen92@users.noreply.github.com>
2023-11-28 20:25:25 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:24,
+       margin:24,
         nav:true,
         dots: false,
         responsive:{
